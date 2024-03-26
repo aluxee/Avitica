6918dceba0261dba7789ec108fc61d3ba6bb3c9d
ava.luxe <ajrluxe@gmail.com>
2024-03-26 18:21:49 -0500

    part one of phase three backend adding user table migration and seeds

---
+++ backend/db/models/user.js
@@ -0,0 +1,71 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [6, 20],
+        hasNoSymbols(value) {
+          if (value == '!@#$%^&*(){}_-+=[]{}:;"' || value == "<,>.?/'") {
+            throw new Error('Username cannot contain any symbols')
+          }
+        },
+        isNotEmail(value) {
+          validator.isEmail(value) ? (function
+            () {
+            throw new Error('Username cannot be an email')
+          }()) : ''
+        }
+      }
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6, 30],
+        isAlpha: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [5, 250],
+        isEmail: true
+      }
+
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['email', 'password', 'createdAt', 'updatedAt']
+      }
+    }
+  });
+  return User;
+};

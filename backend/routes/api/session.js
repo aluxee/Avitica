cd52a8751ed49c2b70990b766c75e1452c726010
ava.luxe <ajrluxe@gmail.com>
2024-03-27 11:25:19 -0500

    part four phase four complete: added backend endpoint to get curr user sess

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -60,6 +60,24 @@ router.delete(
 
 
 
+// Restore session user
+router.get(
+	'/',
+	(req, res) => {
+		const { user } = req;
+		if (user) {
+			const safeUser = {
+				id: user.id,
+				email: user.email,
+				username: user.username,
+				displayName: user.displayName,
+			};
+			return res.json({
+				user: safeUser
+			});
+		} else return res.json({ user: null });
+	}
+);
 
 
 

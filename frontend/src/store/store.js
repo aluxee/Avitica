810dc4dde7868b75500dc288b8c674a77d3c41d3
ava.luxe <ajrluxe@gmail.com>
2024-03-31 18:29:07 -0500

    front end phase 0 additions

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,12 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-
-
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
-
+	// session: sessionReducer
 });
 
 let enhancer;
-
 if (import.meta.env.MODE === "production") {
 	enhancer = applyMiddleware(thunk);
 } else {

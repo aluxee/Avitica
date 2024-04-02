269cd6661485d9d0a61f8914d9ada73675219e83
ava.luxe <ajrluxe@gmail.com>
2024-04-01 19:51:52 -0500

    added loading spinner, complete p1 frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,64 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import * as sessionActions from './store/session';
+import React from 'react';
+// import Loader from 'react-loader-spinner';
+
+
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      {!isLoaded ?
+        <div className='loading'>
+          <div className='loading-spinner'>...</div>
+        </div>
+
+        : (
+          <div></div>
+        )
+      }
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <h1>Welcome!</h1>
-  },
-  {
-    path: '/login',
-    element: <LoginFormPage />
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: '/login',
+        element: <LoginFormPage />
+      }
+    ]
   }
 ]);
 
+
 function App() {
-  return <RouterProvider router={router} />;
+
+  return (
+    <div>
+      < RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App;
